54aea8afe0a321f9a3a329aa39012dfc21700a93
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-02-14 20:05:36 +0000

    make companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,6 +4,10 @@ class CompaniesController < ApplicationController
   # def index
   # end
 
+    def index
+      @companies = Company.all
+    end
+
   def show
     @company = Company.find_by({ "id" => params["id"] })
   end
