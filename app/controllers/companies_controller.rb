bbba07873558bb2b8422f8d0f8bef52be2a2e02b
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-02-14 22:21:00 +0000

    fix company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,28 +1,25 @@
 class CompaniesController < ApplicationController
 
-  # TODO:
-  # def index
-  # end
-
-    def index
-      @companies = Company.all
-    end
-
   def show
-    @company = Company.find_by({ "id" => params["id"] })
+    @contact = Contact.find_by({ "id" => params["id"] })
+    @company = Company.find_by({ "id" => @contact["company_id"] })
   end
 
   def new
-    @company = Company.new
+    @contact = Contact.new
+    @company = Company.find_by({ "id" => params["company_id"] })
+    @contact["company_id"] = @company["id"]
   end
 
   def create
-    @company = Company.new
-    @company["name"] = params["company"]["name"]
-    @company["city"] = params["company"]["city"]
-    @company["state"] = params["company"]["state"]
-    @company.save
-    redirect_to "/companies"
+    @contact = Contact.new
+    @contact["first_name"] = params["contact"]["first_name"]
+    @contact["last_name"] = params["contact"]["last_name"]
+    @contact["email"] = params["contact"]["email"]
+    @contact["phone_number"] = params["contact"]["phone_number"]
+    @contact["company_id"] = params["contact"]["company_id"]
+    @contact.save
+    redirect_to "/companies/#{@contact["company_id"]}"
   end
 
 end
